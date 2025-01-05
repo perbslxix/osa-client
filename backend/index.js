10bae72ec9d2a90f9113e6367bca1920bebf9ffc
Renan <112241110+PerbsLXIX@users.noreply.github.com>
2025-01-05 15:27:41 +0800

    added query

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,9 @@ import mysql from 'mysql'
 import cors from 'cors'
 
 const app = express()
-
 app.use(cors())
 
-mysql.createConnection({
+const db = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'1234',
@@ -15,6 +14,37 @@ mysql.createConnection({
 
 app.get('/', (req, res) => {
     res.json("Hello backend")
+})
+
+app.get('/accreditation', (req, res) => {
+    const q = "SELECT * FROM accreditation"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post('/accreditation', (req, res) => {
+    const q = "INSERT INTO accreditaiton (`accre_id`,`org_id`,`act_id`,`appendices`) VALUES (?)"
+    const value = [
+        req.body.accre_id,
+        req.body.desc,
+        req.body.act_id,
+        req.body.appendices
+    ]
+
+    db.query(q, [value], (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.get('/', (req, res) => {
+    const q = "SELECT * FROM accreditation"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
 })
 
 app.listen(8800, () => {
