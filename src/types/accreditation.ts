8c48b406a7aa2e542e670be43ff5ce938d778933
Renan <112241110+PerbsLXIX@users.noreply.github.com>
2025-01-25 11:17:24 +0800

    changes

--- src/types/accreditation.ts
+++ src/types/accreditation.ts
@@ -1,43 +1,46 @@
-export interface MembersType{
-    name:string;
-    position:string;
-    contactNumber:string;
-    studentNumber:string;
-}
-
-export interface ActivityType{
-    activity:string;
-    learningOutcome:string;
-    targetTime:string;
-    targetGroup:string;
-    personsInvolved:string;
-}
-
-export interface FinancialReportsType{
-    title:string;
-    dateAndTime:string;
-    totalBudget:string;
-}
-
-export interface AccreditationType{
-    constitutionsAndByLaws:string;
-    organizationName:string;
-    type:string;
-    members:Array<MembersType>;
-    planActivities:Array<ActivityType>;
-    letter:string;
-    appendices:string;
-}
-
-export interface SourceOfFundsType{
-    source:string;
-    particulars:string;
-}
-
-export interface BudgetAllocationType{
-    source:string;
-    quantity:string;
-    unitPrice:string;
-    amount:string;
-    receipt:string;
-}
+  export interface MembersType {
+    email: string
+    name: string;
+    position: string;
+    contactNumber: string;
+    studentNumber: string;
+  }
+  
+  export interface ActivityType {
+    activity: string;
+    learningOutcome: string;
+    targetTime: string;
+    targetGroup: string;
+    personsInvolved: string;
+  }
+  
+  export interface FinancialReportsType {
+    title: string;
+    dateAndTime: string;
+    totalBudget: string;
+  }
+  
+  export interface AccreditationType {
+    constitutionsAndByLaws: string;
+    organizationName: string;
+    type: string;
+    members: Array<MembersType>;
+    officers: Array<MembersType>
+    planActivities: Array<ActivityType>;
+    letter: string;
+    appendices: string;
+    membersFile: string;
+  }
+  
+  export interface SourceOfFundsType {
+    source: string;
+    particulars: string;
+  }
+  
+  export interface BudgetAllocationType {
+    source: string;
+    quantity: string;
+    unitPrice: string;
+    amount: string;
+    receipt: string;
+  }
